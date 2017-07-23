32ab29c929e053c53515eb73c7efa740a963fbe7
Mikko Vuorinen <vuorinem@gmail.com>
2017-07-23 20:20:41 +0100

    Add login button to front page

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import { autoinject } from "aurelia-framework";
+import { AuthService, UserService } from "./auth/index";
+// Note: TS would allow simplifying import to "./auth", but current bundler doesn't supprot it
+
+@autoinject
 export class App {
-  message = 'Hello World!';
+  constructor(
+    private authService: AuthService,
+    private userService: UserService) {
+  }
+
+  private async login() {
+    await this.authService.login();
+  }
 }
