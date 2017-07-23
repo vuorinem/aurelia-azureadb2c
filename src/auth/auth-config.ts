89ba28d9f6e1d4c26c977f08a567e13b5cb0008f
Mikko Vuorinen <vuorinem@gmail.com>
2017-07-23 20:19:54 +0100

    Add AD config and implement login

---
+++ src/auth/auth-config.ts
@@ -0,0 +1,7 @@
+export default {
+    adTenantName: '[MyAdTenant].onmicrosoft.com',
+    adPolicyName: '',
+    adClientId: '',
+    adRedirectUri: 'http://localhost:9000',
+    scopes: 'openid https://[MyAdTenant].onmicrosoft.com/[MyAppName]/[Scope]',
+}
--- src/auth/auth-service.ts
+++ src/auth/auth-service.ts
@@ -1,2 +1,58 @@
+import * as hello from "hellojs";
+import authConfig from "./auth-config";
+
 export class AuthService {
+    private isAuthenticated: boolean = false;
+    private name: string;
+
+    constructor() {
+        // Configure Azure AD B2C module for hello.js
+        hello.init({
+            azureAD: {
+                oauth: {
+                    version: 2,
+                    auth: this.getAuthUrl(authConfig.adTenantName, authConfig.adPolicyName),
+                },
+                scope_delim: ' ',
+            }
+        });
+
+        // Configure application details
+        hello.init({ azureAD: authConfig.adClientId }, {
+            redirect_uri: authConfig.adRedirectUri,
+            scope: authConfig.scopes,
+            response_type: 'token',
+            display: 'page',
+        });
+
+        this.handleAuthenticationResponse();
+    }
+
+    public async login() {
+        hello('azureAD').login()
+
+        return hello.login('azureAD', {}, () => {
+            this.handleAuthenticationResponse()
+        });
+    }
+
+    private handleAuthenticationResponse() {
+        const response = hello('azureAD').getAuthResponse();
+
+        if (!response) {
+            this.isAuthenticated = false;
+            return;
+        }
+
+        if (!('access_token' in response) || !response.access_token) {
+            this.isAuthenticated = false;
+            return;
+        }
+
+        this.isAuthenticated = true;
+    }
+
+    private getAuthUrl(tenant: string, policy: string) {
+        return `https://login.microsoftonline.com/tfp/${tenant}/${policy}/oauth2/v2.0/authorize`;
+    }
 }
