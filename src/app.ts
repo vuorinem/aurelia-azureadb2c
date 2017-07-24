d1a24bf243a16163c504406425801f18dc5df78c
Mikko Vuorinen <vuorinem@gmail.com>
2017-07-24 02:28:00 +0100

    Add Api call
    Configure fetch client with the auth interceptor.
    Add button to make an Api call.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import { autoinject } from "aurelia-framework";
+import { HttpClient } from "aurelia-fetch-client";
 import { AuthService } from "./auth/auth-service";
 // Note: TS would allow simplifying import to "./auth", but current bundler doesn't supprot it
 
 @autoinject
 export class App {
-  constructor(private authService: AuthService) {
+  private apiResponses: Response[] = [];
+
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService) {
+
+    this.httpClient.configure(config => 
+      config.withInterceptor(this.authService.getAuthenticationInterceptor()));
   }
 
   private async login() {
@@ -13,5 +21,9 @@ export class App {
 
   private async logout() {
     await this.authService.logout();
+  }
+
+  private async callApi() {
+    this.apiResponses.push(await this.httpClient.fetch('/endpoint'));
   }
 }
